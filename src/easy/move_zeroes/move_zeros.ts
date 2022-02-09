abb6cd97e3be67f7fafcf55f96654e6750592c25
Cosmin <atok3th@gmail.com>
2022-02-09 17:08:43 +0200

    Solve Move zeros

--- src/easy/move_zeroes/move_zeros.ts
+++ src/easy/move_zeroes/move_zeros.ts
@@ -6,29 +6,23 @@ function moveZeroes(nums: number[]): void {
   }
 
   const length: number = nums.length;
-
   let leftPointer: number = 0;
-  let rightPointer: number = length - 1;
+  let rightPointer: number = 0;
 
-  while (leftPointer <= rightPointer) {
-    const leftNumber = nums[leftPointer];
-    const rightNumber = nums[rightPointer];
-    if (isZero(leftNumber)) {
-      nums.splice(leftPointer, 1);
-      nums.push(leftNumber);
+  while (rightPointer <= length - 1) {
+    const number = nums[rightPointer];
+    if (number !== 0) {
+      swapTwoNumbers(nums, leftPointer, rightPointer);
+      leftPointer++;
+      rightPointer++;
+    } else {
+      rightPointer++;
     }
-
-    if (isZero(rightNumber)) {
-      nums.splice(rightPointer, 1);
-      nums.push(rightNumber);
-    }
-    rightPointer--;
-
   }
 }
 
-function isZero(number: number): boolean {
-  return number === 0;
+function swapTwoNumbers(nums: number[], index1: number, index2: number): void {
+  [nums[index1], nums[index2]] = [nums[index2], nums[index1]];
 }
 
 function main(): void {
@@ -36,8 +30,6 @@ function main(): void {
   const expected: number[] = [1, 3, 12, 0, 0];
 
   moveZeroes(input);
-  console.log(`Result: ${input}`);
-
   assert(input.length === expected.length);
   for (const i in expected) {
     assert(input[i] === expected[i]);
